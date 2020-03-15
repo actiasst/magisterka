828c5990490e22abc37d28dfd9cd776f2a234525
actiasst <maciekactias@gmail.com>
2020-03-15 08:16:35 +0100

    Add files via upload

--- exporter.py
+++ exporter.py
@@ -3,19 +3,29 @@
 ##############################################################
 
 def addVertex(x,y,z):
-	myFile.write('v ' + str(x)+ ' ' + str(y) + ' ' + str(z) + '\n')
+	myFile.write(str(x)+ ' ' + str(y) + ' ' + str(z) + '\n')
 
 def addTriangle(v1,v2,v3):
-	myFile.write('f ' + str(v1)+ ' ' + str(v2) + ' ' + str(v3) + '\n')
+	myFile.write(str(v1)+ ' ' + str(v2) + ' ' + str(v3) + '\n')
 
 def addTexture(u1):
-	myFile.write('u ' + str(u1) + '\n')
+	myFile.write(str(u1) + '\n')
 
 def exportCOORD(step,frame,node):
 	addVertex(odb.steps[step].frames[frame].fieldOutputs["COORD"].values[node].data[0], odb.steps[step].frames[frame].fieldOutputs["COORD"].values[node].data[1], odb.steps[step].frames[frame].fieldOutputs["COORD"].values[node].data[2])
 
 def exportU(step,frame,node,orientation):
 	myFile.write(str(odb.steps[step].frames[frame].fieldOutputs["U"].values[node].data[orientation])+'\n')
+	
+def exportELEMENTS(node):
+	myFile.write(str(odb.rootAssembly.elementSets[" ALL ELEMENTS"].elements[0][node].connectivity[0] - 1) + ' ' +
+	str(odb.rootAssembly.elementSets[" ALL ELEMENTS"].elements[0][node].connectivity[1] - 1) + ' ' +
+	str(odb.rootAssembly.elementSets[" ALL ELEMENTS"].elements[0][node].connectivity[2] - 1) + ' ' +
+	str(odb.rootAssembly.elementSets[" ALL ELEMENTS"].elements[0][node].connectivity[3] - 1) + ' ' +
+	str(odb.rootAssembly.elementSets[" ALL ELEMENTS"].elements[0][node].connectivity[4] - 1) + ' ' +
+	str(odb.rootAssembly.elementSets[" ALL ELEMENTS"].elements[0][node].connectivity[5] - 1) + ' ' +
+	str(odb.rootAssembly.elementSets[" ALL ELEMENTS"].elements[0][node].connectivity[6] - 1) + ' ' +
+	str(odb.rootAssembly.elementSets[" ALL ELEMENTS"].elements[0][node].connectivity[7] - 1) + '\n')
 
 ##############################################################
 #######actual exporter
@@ -32,6 +42,7 @@ step = "Apply Load"
 fileName = 'skrypty\\magisterka\\exported\\'
 frame = 1
 nodesCounter = len(odb.steps[step].frames[frame].fieldOutputs["COORD"].values)
+elementsCounter = len(odb.rootAssembly.elementSets[" ALL ELEMENTS"].elements[0])
 
 print "STARTING EXPORTING"
 
@@ -73,6 +84,15 @@ myFile = open(fileName+'scale_factor.txt','w')
 myFile.write(str(session.viewports["Viewport: 1"].odbDisplay.commonOptions.autoDeformationScaleValue))
 myFile.close()
 print "SCALE FACTOR EXPORTED"
+print " "
+
+
+print "EXPORTING ELEMENTS"
+myFile = open(fileName+'ELEMENTS.txt','w')
+for node in range(elementsCounter):
+	exportELEMENTS(node)
+myFile.close()
+print "ELEMENTS EXPORTED"
 print " "
 
 
